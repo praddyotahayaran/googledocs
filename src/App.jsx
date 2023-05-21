21142a1da4d77b6c7812c13ed4829f714a5d945f
Praddyota <praddyotahayaran100@gmail.com>
2023-05-21 15:32:56 +0530

    checking... deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css'
-import Header from './Components/Header/Header'
-import TextEditor from './Components/TextEditor/TextEditor'
+// import Header from './Components/Header/Header'
+// import TextEditor from './Components/TextEditor/TextEditor'
 
 function App() {
 
   return (
     <div className="app">
-      <Header />
-      <TextEditor />
-    
+      {/* <Header />
+      <TextEditor /> */}
+    hello
     </div>
   )
 }
